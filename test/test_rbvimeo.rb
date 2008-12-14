3c96ee52a956d6badf8c2d7fec5a9da1e4c80fcd
Matt Pruitt <guitsaru@gmail.com>
2008-12-14 17:26:55 -0600

    Don't connect to the vimeo server in any tests.

--- test/test_rbvimeo.rb
+++ test/test_rbvimeo.rb
@@ -18,7 +18,7 @@ class TestRbVimeo < Test::Unit::TestCase
       assert_equal("09cc6d8b963c73caf647e436b2147810", @vimeo.generate_signature(@params))
     end
     
-    should "genearte a url" do
+    should "generate a url" do
       assert_equal("http://www.vimeo.com/api/rest?api_key=#{@api_key}&method=vimeo.videos.getInfo&video_id=339189&api_sig=09cc6d8b963c73caf647e436b2147810", @vimeo.generate_url(@params, "read"))
     end
     
@@ -38,6 +38,11 @@ class TestRbVimeo < Test::Unit::TestCase
     end
     
     should "return an xml file" do
+      test_video_xml_file = File.join(File.dirname(__FILE__), %w[XML/339189.xml])
+
+      video_hpricot = open(test_video_xml_file) {|file| Hpricot(file)}
+      @vimeo.stubs(:get_xml).returns(video_hpricot)
+      
       assert_not_nil(@vimeo.get_xml(@vimeo.generate_url(@params, "read")).at('video'))
     end
   end
